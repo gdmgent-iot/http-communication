67825316c831dd1409ad5774d9ae7f4aed704527
Frederick Roegiers <frederick@rogerthat.be>
2024-10-01 10:43:18 +0200

    wip

--- arduino/polling/polling.ino
+++ arduino/polling/polling.ino
@@ -1,6 +1,28 @@
+// import external libraries
+#include <WiFiNINA.h>
+#include <ArduinoHttpClient.h>
+#include <ArduinoJson.h>
+
+// include local credentials (added in gitignore, thus not in repo)
+#include "secrets.h"
+
+// local wifi credentials
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+// server settings
+char serverAddres[] = "";
+int port = 3001;
+
+// wifi settings
+int status = WL_IDLE_STATUS;
+WiFiClient wifi;
+// HttpClient client = HttpClient(wifi, serverAddress, port);
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
